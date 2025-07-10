3eb7570e4470efd1ce36a147c489f903b3f2e764
Aleksandar <aleksandar@avasilic.com>
2025-07-10 09:51:46 +0900

    Add goto formula v1.4.1

---
+++ Formula/goto.rb
@@ -0,0 +1,15 @@
+class Goto < Formula
+  desc "Simple CLI SSH manager"
+  homepage "https://github.com/grafviktor/goto"
+  url "https://github.com/grafviktor/goto/releases/download/v1.4.1/goto-v1.4.1.zip"
+  sha256 "bb49dd2e1491adf86c9a0e6f7f3c7e5579d3f7da213f9375fd267d13ba6cc6ca"
+  license "MIT"
+
+  def install
+    bin.install "goto"
+  end
+
+  test do
+    assert_match "goto", shell_output("#{bin}/goto --help")
+  end
+end
